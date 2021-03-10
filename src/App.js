8c0c3c0d6f963c5b4c3209eb8c2d655fddc632c6
Sem Bauke <semboot699@gmail.com>
2021-03-10 11:52:35 +0100

    feature(visual) initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Introduction from './components/Introduction'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Introduction.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+class Introduction extends react.Component {
+    render(){
+        return <h1>Welcome to Visual CSS</h1>
+    }
+}
+
+export default Introduction
